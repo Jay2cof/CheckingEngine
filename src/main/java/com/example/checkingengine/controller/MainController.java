e825993ac0795d16d4a8078b35dae67ce2c387f3
Jay2cof <justice101onuoha@gmail.com>
2024-04-08 13:02:31 +0200

    Booking Engine connecting both fxml files

--- src/main/java/com/example/checkingengine/controller/MainController.java
+++ src/main/java/com/example/checkingengine/controller/MainController.java
@@ -1,8 +1,6 @@
 package com.example.checkingengine.controller;
 
-<<<<<<< HEAD
-public class MainController {
-=======
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,28 +8,28 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class MainController{
+public class MainController {
+
+
+
+public class MainController {
     @FXML
     public Button btn_login;
     @FXML
     public Button btn_register;
 
     @FXML
-    public void registerNewCustomer(Stage registration){
+    public void registerNewCustomer(Stage registration) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
             Scene scene = new Scene(root);
             registration.setScene(scene);
             registration.setTitle("Registration Page");
             registration.show();
-        }
-        catch (Exception J){
+        } catch (Exception J) {
             J.printStackTrace();
         }
 
     }
-
-
-
->>>>>>> 48a08de (Booking Engine connecting both fxml files)
+}
 }
