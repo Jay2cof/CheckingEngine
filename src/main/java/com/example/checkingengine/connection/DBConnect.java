1e1e0e584ff77e844d8d37869c6f0bfb05a1ac12
Jay2cof <justice101onuoha@gmail.com>
2024-04-18 10:52:26 +0200

    Booking Engine connecting both fxml files connecting to the database

--- src/main/java/com/example/checkingengine/connection/DBConnect.java
+++ src/main/java/com/example/checkingengine/connection/DBConnect.java
@@ -7,8 +7,12 @@ import java.sql.SQLException;
 
 public class DBConnect {
     public static Connection connect() throws SQLException {
+<<<<<<< HEAD
         Connection con = DriverManager.getConnection("jdbc://localhost/checkingengine",
                 "root","onu101cj");
+=======
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/bookingsystem", "root","onu101cj");
+>>>>>>> 6a25524 (Booking Engine connecting both fxml files connecting to the database)
         return con;
 
     }
